514fc0e8fd0a9f62cdb3462caff88f85e33055eb
Jihoon Lee <jihoon@kodebox.io>
2021-01-12 00:42:17 +0900

    Responsive design

--- themes/zuzu/static/js/reveal.js
+++ themes/zuzu/static/js/reveal.js
@@ -1,9 +1,2 @@
-anime({
-    targets: '.reveal-paper-item',
-    translateY: -8,
-    delay: anime.stagger(100), // increase delay by 100ms for each elements.
-    duration: 3000,
-    loop: true,
-});
 
 
