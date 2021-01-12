807519e8c6c4100b49a18faa2c181de2f4c97beb
Jihoon Lee <jihoon@kodebox.io>
2021-01-12 15:51:12 +0900

    Add default alt tag

--- docs/js/attribute.js
+++ docs/js/attribute.js
@@ -4,3 +4,9 @@ document.getElementsByTagName("H3")[0].setAttribute("role", "heading");
 document.getElementsByTagName("H4")[0].setAttribute("role", "heading");
 document.getElementsByTagName("H5")[0].setAttribute("role", "heading");
 document.getElementsByTagName("p")[0].setAttribute("role", "paragraph");
+
+$('img').each(function () {
+    var $img = $(this);
+    var filename = $img.attr('src')
+    $img.attr('alt', filename.substring(0, filename.lastIndexOf('.')));
+});
